7c6a82d5ad4681df65340a856ba3b5b46e182dcd
Shubham <shubhamcu27@gmail.com>
2023-11-05 17:56:35 -0800

    first commit, with Signin and Signup functionality

---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,89 @@
+import { ID, Query } from "appwrite";
+import { INewUser } from "@/types";
+import { account, appwriteConfig, avatars, databases } from "./config";
+
+export async function createUserAccount(user: INewUser) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    );
+    if (!newAccount) throw new Error("Account creation failed");
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+}
+
+export async function saveUserToDB(user: {
+  accountId: string;
+  name: string;
+  email: string;
+  username?: string;
+  imageUrl?: URL;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+    return newUser;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function signInAccount(user: { email: string; password: string }) {
+  try {
+    const session = await account.createEmailSession(user.email, user.password);
+    return session;
+  } catch (err) {
+    console.error(err);
+    // return err;
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await account.get();
+
+    if (!currentAccount) throw new Error("No account found");
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw new Error("No user found");
+
+    return currentUser.documents[0];
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function signOutAccount() {
+  try {
+    const session = await account.deleteSession("current");
+    return session;
+  } catch (err) {
+    console.error(err);
+  }
+}
